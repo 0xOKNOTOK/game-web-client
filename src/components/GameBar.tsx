49d87305bf98e308c7784da8f985a7b13b4d1bc1
Ross Greenall <rossgreenall@gmail.com>
2021-11-23 00:45:12 +0000

    Changed header p tags to buttons for future functionality.

--- src/components/GameBar.tsx
+++ src/components/GameBar.tsx
@@ -14,6 +14,36 @@ export const GameBar: React.FC = (): React.ReactElement => {
           'https://i.pinimg.com/originals/37/eb/8a/37eb8a771ceb2abb5d3f6efff312d4cf.png'
         }
       />
+      <GameIcon
+        name='Sky City'
+        image={
+          'https://i.pinimg.com/originals/37/eb/8a/37eb8a771ceb2abb5d3f6efff312d4cf.png'
+        }
+      />
+      <GameIcon
+        name='Sky City'
+        image={
+          'https://i.pinimg.com/originals/37/eb/8a/37eb8a771ceb2abb5d3f6efff312d4cf.png'
+        }
+      />
+      <GameIcon
+        name='Sky City'
+        image={
+          'https://i.pinimg.com/originals/37/eb/8a/37eb8a771ceb2abb5d3f6efff312d4cf.png'
+        }
+      />
+      <GameIcon
+        name='Sky City'
+        image={
+          'https://i.pinimg.com/originals/37/eb/8a/37eb8a771ceb2abb5d3f6efff312d4cf.png'
+        }
+      />
+      <GameIcon
+        name='Sky City'
+        image={
+          'https://i.pinimg.com/originals/37/eb/8a/37eb8a771ceb2abb5d3f6efff312d4cf.png'
+        }
+      />
     </header>
   )
 }

0a9c2f6ca7bbeba224a8f6fed33a4061ab9209e2
Ross Greenall <rossgreenall@gmail.com>
2021-11-25 23:57:39 +0000

    Started redesigning news article component.

--- src/components/NewsArticle.tsx
+++ src/components/NewsArticle.tsx
@@ -4,13 +4,11 @@ import { Container } from './Container'
 export const NewsArticle: React.FC = (): React.ReactElement => {
   return (
     <Container class='news-article'>
-      <article>
-        <img />
-        <figcaption>
-          <h4>Update 1.0.3</h4>
-          <p>New mounts, items and raids! View the latest patch notes at...</p>
-        </figcaption>
-      </article>
+      <img />
+      <figcaption>
+        <h4>Update 1.0.3</h4>
+        <p>New mounts, items and raids! View the latest patch notes at...</p>
+      </figcaption>
     </Container>
   )
 }

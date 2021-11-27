5f3cb3105513fea81e4803572f54bf2c9185bef5
Ross Greenall <rossgreenall@gmail.com>
2021-11-27 19:53:12 +0000

    Replaced template text with temporary logo to style properly.

--- src/components/CurrentGame.tsx
+++ src/components/CurrentGame.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ButtonIcon } from './ButtonIcon'
 import { ButtonLarge } from './ButtonLarge'
 import { Container } from './Container'
+import wowWotlk from '../assets/wow_wotlk_placeholder.png'
 
 interface Props {
   game: Game
@@ -21,7 +22,9 @@ export const CurrentGame: React.FC<Props> = ({ game }): React.ReactElement => {
   return (
     <div>
       <Container class='current-game'>
-        <h2>GAME_ICON</h2>
+        <div className='game-play-top'>
+          <img src={wowWotlk} alt='Current game logo.' />
+        </div>
 
         <div className='game-play-bottom'>
           <h4>{game.name}</h4>

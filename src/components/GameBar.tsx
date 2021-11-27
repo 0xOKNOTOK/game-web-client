414096d4985cd13371b4d3e8cfb552d52a610faf
Ross Greenall <rossgreenall@gmail.com>
2021-11-27 20:00:33 +0000

    Included a new add game button to add new games.

--- src/components/GameBar.tsx
+++ src/components/GameBar.tsx
@@ -5,44 +5,56 @@ export const GameBar: React.FC = (): React.ReactElement => {
   return (
     <header className='gameBar'>
       <GameIcon
+        class='game-icon'
         name='Underground'
         image='https://i.pinimg.com/originals/37/eb/8a/37eb8a771ceb2abb5d3f6efff312d4cf.png'
       />
       <GameIcon
+        class='game-icon'
         name='Sky City'
         image={
           'https://i.pinimg.com/originals/37/eb/8a/37eb8a771ceb2abb5d3f6efff312d4cf.png'
         }
       />
       <GameIcon
+        class='game-icon'
         name='Sky City'
         image={
           'https://i.pinimg.com/originals/37/eb/8a/37eb8a771ceb2abb5d3f6efff312d4cf.png'
         }
       />
       <GameIcon
+        class='game-icon'
         name='Sky City'
         image={
           'https://i.pinimg.com/originals/37/eb/8a/37eb8a771ceb2abb5d3f6efff312d4cf.png'
         }
       />
       <GameIcon
+        class='game-icon'
         name='Sky City'
         image={
           'https://i.pinimg.com/originals/37/eb/8a/37eb8a771ceb2abb5d3f6efff312d4cf.png'
         }
       />
       <GameIcon
+        class='game-icon'
         name='Sky City'
         image={
           'https://i.pinimg.com/originals/37/eb/8a/37eb8a771ceb2abb5d3f6efff312d4cf.png'
         }
       />
       <GameIcon
+        class='game-icon'
         name='Sky City'
         image={
           'https://i.pinimg.com/originals/37/eb/8a/37eb8a771ceb2abb5d3f6efff312d4cf.png'
         }
+      />
+      <GameIcon
+        class='add-game'
+        name='Add Game'
+        image='https://i.imgur.com/jIUM1yq.png'
       />
     </header>
   )
--- src/components/GameIcon.tsx
+++ src/components/GameIcon.tsx
@@ -3,12 +3,13 @@ import React from 'react'
 interface Props {
   name: string
   image: string
+  class: string
 }
 
-export const GameIcon: React.FC<Props> = ({ name, image }) => {
+export const GameIcon: React.FC<Props> = props => {
   return (
     <figure>
-      <img className='gameicon' src={image} alt={name} />
+      <img className={props.class} src={props.image} alt={props.name} />
     </figure>
   )
 }

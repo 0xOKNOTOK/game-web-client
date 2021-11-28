87be4e92b2896b51e566ace1ebc63dc84368e8f4
Ross Greenall <rossgreenall@gmail.com>
2021-11-28 00:20:12 +0000

    Created temporary articles to populate the latest updates section.

--- src/components/NewsArticle.tsx
+++ src/components/NewsArticle.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import { Container } from './Container'
+import newsTemplate from '../assets/news.jpg'
+import { ButtonLarge } from './ButtonLarge'
 
 export const NewsArticle: React.FC = (): React.ReactElement => {
+  const handleArticleClick = (e: React.MouseEvent) => {
+    e.preventDefault()
+  }
   return (
     <Container class='news-article'>
-      <img />
+      <img src={newsTemplate} alt='Mount' />
       <figcaption>
-        <h4>Update 1.0.3</h4>
-        <p>New mounts, items and raids! View the latest patch notes at...</p>
+        <h4>Shadowlands: Watch the Eternity’s End Developer Preview</h4>
+        <p>
+          Zovaal, the elusive Jailer and ruler of the Maw in the Shadowlands,
+          has made his next move—and with it, he threatens to rewrite the rules
+          of reality.
+        </p>
+        <ButtonLarge buttonLabel='Learn More...' onClick={handleArticleClick} />
       </figcaption>
     </Container>
   )

bf8c5c7d68edac3d079e045a517401f2f3ae3a13
Ross Greenall <rossgreenall@gmail.com>
2021-11-30 23:11:28 +0000

    Added loginPost to userService as a start to authentication.

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,1 +1,11 @@
-export {}
+import axios  from 'axios'
+
+const API_URL = ''
+
+const loginPost: Function = async (credentials: object) => {
+    const request = await axios.get(API_URL, credentials);
+    return request.data;
+
+}
+
+export default loginPost

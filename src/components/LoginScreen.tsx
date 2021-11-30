563788408e942717f54369dd88b8c30b053dd10c
Ross Greenall <rossgreenall@gmail.com>
2021-11-30 19:15:13 +0000

    Installed Formik to better handle form data. Created login screen with template Formik form.

---
+++ src/components/LoginScreen.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Container } from './Container'
+import {
+  Formik,
+  FormikHelpers,
+  FormikProps,
+  Form,
+  Field,
+  FieldProps,
+} from 'formik'
+import { LanguageServiceMode } from 'typescript'
+
+interface loginForm {
+  username: string
+  password: string
+}
+
+export const LoginScreen: React.FC<{}> = (): React.ReactElement => {
+  const initialValues: loginForm = { username: '', password: '' }
+  return (
+    <Container class='login-screen'>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values, actions) => {
+          console.log({ values, actions })
+          alert(JSON.stringify(values, null, 2))
+          actions.setSubmitting(false)
+        }}
+      >
+        <Form>
+          <label htmlFor='username'>Username</label>
+          <Field id='username' name='username' placeholder='Username' />
+          <label htmlFor='password'>Password</label>
+          <Field
+            id='password'
+            name='password'
+            placeholder='Password'
+            type='password'
+          />
+          <button type='submit'>Log in</button>
+        </Form>
+      </Formik>
+    </Container>
+  )
+}

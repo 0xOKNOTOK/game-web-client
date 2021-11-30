4cbcef3a37c7aa1a3bff846259473c6f5690449e
Ross Greenall <rossgreenall@gmail.com>
2021-11-30 23:18:00 +0000

    Started building request functionality for adding, collecting and returning games in a users library.

---
+++ src/services/gamesService.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+
+const API_URL_LIBRARY: string = "";
+
+let token: string | null = null;
+
+const setToken: Function = (newToken: String) => {
+    token = `bearer ${newToken}`
+}
+
+const getUsersGames: Function = async () => {
+    const request = await axios.get(API_URL_LIBRARY);
+    return request.data;
+}
+
+const addNewGameFromGameList: Function = async (game: Object) =>{
+    const cfg: Object = {
+        headers: {
+            Authorization: token
+        }
+    } 
+    const request =  await axios.post(API_URL_LIBRARY, game, cfg)
+    return request.data;
+}
